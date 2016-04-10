f233efbe729a1dd16673936d47cc914cbd520c5b
seitti <norinacsizmadia@gmail.com>
2016-04-10 10:52:15 +0200

    Added json file

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,11 +1,16 @@
+import json
 from django.shortcuts import render
 # from .models import Message
 
+f = open(r'c:\pylvax\django\project\starter_app\new_content.json', 'r')
+data = json.load(f)
 
 def home(request):
     # messages = Message.objects.order_by('order')
-    messages = ['Első', 'Második', 'Harmadik']
-    context_dict = {
-        'messages': messages
-    }
+    # messages = ['Első', 'Második', 'Harmadik']
+    # context_dict = {
+        # 'messages': messages
+    # }
+    context_dict = data
+
     return render(request, 'starter_app/home.html', context_dict)
