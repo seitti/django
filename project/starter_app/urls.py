d402d0b5de7da0958f07cefb5b952a1c930fc7d6
seitti <norinacsizmadia@gmail.com>
2016-04-19 17:15:10 +0200

    blog is created

--- project/starter_app/urls.py
+++ project/starter_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-]
+    url(r'^blog/$', views.blog, name='blog'),
+    # url(r'^blog/post/$', views.post, name='post'),
+    url(r'^blog/.+/$', views.post, name='post'),
+    ]
--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,9 +1,22 @@
-import json
+import json, os
 from django.shortcuts import render
+from config.settings.base import DJANGO_ROOT
+from django.template.defaultfilters import slugify
 # from .models import Message
 
-f = open(r'c:\pylvax\django\project\starter_app\new_content.json', 'r')
-data = json.load(f)
+json_path = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'new_content.json')
+f = open(json_path, 'r')
+data = json.load(f)['my_string']
+f.close()
+
+json_path_posts = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'posts.json')
+file_posts = open(json_path_posts, 'r')
+data_posts = json.load(file_posts)
+file_posts.close()
+
+for item in data_posts:
+	data_posts[item]['slug'] = slugify(data_posts[item]['title'])
+
 
 def home(request):
     # messages = Message.objects.order_by('order')
@@ -11,6 +24,21 @@ def home(request):
     # context_dict = {
         # 'messages': messages
     # }
-    context_dict = data
-
+    context_dict = {'messages' : data}
     return render(request, 'starter_app/home.html', context_dict)
+
+
+def blog(request):
+    posts = {'posts' : data_posts}
+    return render(request, 'blog.html', posts)
+
+
+def post(request):
+	url_slug = str(request)[25:-3]
+	for item in data_posts:
+		if url_slug == data_posts[item]['slug']:
+			posts = {'title' : data_posts[item]['title'],
+					'date' : data_posts[item]['date'],
+					'text' : data_posts[item]['text']}
+
+	return render(request, 'post.html', posts)
