7120e4b85555f00dab45672bcadcc21d06f908a4
seitti <norinacsizmadia@gmail.com>
2016-05-11 19:54:42 +0200

    new_homepage

--- project/starter_app/urls.py
+++ project/starter_app/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^posts/$', views.blog, name='blog'),
-    # url(r'^blog/post/$', views.post, name='post'),
-    # url(r'^posts/.+/$', views.post, name='post'),
+    url(r'^posts/$', views.home, name='home'),
     url(r'^posts/(?P<slug>[\w-]+)/$', views.post, name='post'),
     ]
--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,55 +1,36 @@
 import json, os
-
 from django.shortcuts import render
 from config.settings.base import DJANGO_ROOT
 from dateutil.parser import parse
 
 
-def publish_date(json_date):
-    date = parse(json_date)
-    return date
-
-
-def read_home_json():
-    json_path = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'new_content.json')
-    f = open(json_path, 'r')
-    home_data_json = json.load(f)['my_string']
-    f.close()
-    return home_data_json
-
-
-def read_blog_json():
-    json_path_posts = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'posts.json')
+def read_blog_homepage():
+    json_path_posts = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'posts_content.json')
     file_posts = open(json_path_posts, 'r')
     data_posts_json = json.load(file_posts)
     file_posts.close()
 
     for item in data_posts_json:
-        item['date'] = publish_date(item['date'])
+        item['date'] = parse(item['date'])
 
     return data_posts_json
 
 
-def post_slug(slug):
-    for item in data_posts:
+def get_post_by_slug(slug):
+    for item in homepage:
         if item['slug'] == slug:
             select_post = item
     return select_post
 
 
 def home(request):
-    context_dict = {'messages' : home_data}
+    context_dict = {'posts' : homepage}
     return render(request, 'starter_app/home.html', context_dict)
 
 
-def blog(request):
-    context_dict = {'posts' : data_posts}
-    return render(request, 'blog.html', context_dict)
-
-
 def post(request, slug):
-    context_dict = post_slug(slug)
-    return render(request, 'post.html', context_dict)
+    context_dict = {'selected_post' : get_post_by_slug(slug), 'posts' : homepage}
+    return render(request, 'starter_app/post.html', context_dict)
 
-home_data = read_home_json()
-data_posts = read_blog_json()
+
+homepage = read_blog_homepage()
