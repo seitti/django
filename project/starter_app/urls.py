88a793b18cd30b2382cd0cf96fc572c60aee0abe
seitti <norinacsizmadia@gmail.com>
2016-05-11 17:15:54 +0200

    corrected blog

--- project/starter_app/urls.py
+++ project/starter_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^blog/$', views.blog, name='blog'),
+    url(r'^posts/$', views.blog, name='blog'),
     # url(r'^blog/post/$', views.post, name='post'),
-    url(r'^blog/.+/$', views.post, name='post'),
+    # url(r'^posts/.+/$', views.post, name='post'),
+    url(r'^posts/(?P<slug>[\w-]+)/$', views.post, name='post'),
     ]
--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,44 +1,81 @@
 import json, os
+
 from django.shortcuts import render
 from config.settings.base import DJANGO_ROOT
-from django.template.defaultfilters import slugify
+from dateutil.parser import parse
+
+# from django.template.defaultfilters import slugify
 # from .models import Message
 
-json_path = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'new_content.json')
-f = open(json_path, 'r')
-data = json.load(f)['my_string']
-f.close()
+def publish_date(json_date):
+    date = parse(json_date)
+    return date
 
-json_path_posts = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'posts.json')
-file_posts = open(json_path_posts, 'r')
-data_posts = json.load(file_posts)
-file_posts.close()
 
-for item in data_posts:
-	data_posts[item]['slug'] = slugify(data_posts[item]['title'])
+def read_home_json():
+    json_path = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'new_content.json')
+    f = open(json_path, 'r')
+    home_data_json = json.load(f)['my_string']
+    f.close()
+    return home_data_json
+
+
+def read_blog_json():
+    json_path_posts = os.path.join(DJANGO_ROOT, 'starter_app', 'contents', 'posts.json')
+    file_posts = open(json_path_posts, 'r')
+    data_posts_json = json.load(file_posts)
+    file_posts.close()
+
+    for item in data_posts_json:
+        item['date'] = publish_date(item['date'])
+
+    return data_posts_json
+
+
+def post_slug(slug):
+    for item in data_posts:
+        if item['slug'] == slug:
+            select_post = item
+    return select_post
+
+#def post_slug(slug):
+#    for item in data_posts:
+#        if data_posts[item]['slug'] == slug:
+#            select_post = data_posts[item]
+#    return select_post
+
+
+# for item in data_posts:
+#    data_posts[item]['slug'] = slugify(data_posts[item]['title'])
 
 
 def home(request):
-    # messages = Message.objects.order_by('order')
-    # messages = ['Első', 'Második', 'Harmadik']
-    # context_dict = {
-        # 'messages': messages
-    # }
-    context_dict = {'messages' : data}
+    context_dict = {'messages' : home_data}
     return render(request, 'starter_app/home.html', context_dict)
+
+        # messages = Message.objects.order_by('order')
+        # messages = ['Első', 'Második', 'Harmadik']
+        # context_dict = {
+            # 'messages': messages
+        # }
 
 
 def blog(request):
-    posts = {'posts' : data_posts}
-    return render(request, 'blog.html', posts)
+    context_dict = {'posts' : data_posts}
+    return render(request, 'blog.html', context_dict)
 
 
-def post(request):
-	url_slug = str(request)[25:-3]
-	for item in data_posts:
-		if url_slug == data_posts[item]['slug']:
-			posts = {'title' : data_posts[item]['title'],
-					'date' : data_posts[item]['date'],
-					'text' : data_posts[item]['text']}
+def post(request, slug):
+    context_dict = post_slug(slug)
+    return render(request, 'post.html', context_dict)
 
-	return render(request, 'post.html', posts)
+    # url_slug = str(request)[26:-3]
+    # for item in data_posts:
+        # if url_slug == data_posts[item]['slug']:
+            #posts = {'title' : data_posts[item]['title'],
+                    # 'date' : data_posts[item]['date'],
+                    # 'text' : data_posts[item]['text']}
+
+
+home_data = read_home_json()
+data_posts = read_blog_json()
